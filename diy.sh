442083954266a4fb810a6ccaa24df46bf213ce2f
3092099 <122336705+3092099@users.noreply.github.com>
2024-12-25 11:12:42 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -6,14 +6,6 @@ repo=${1:-openwrt}
 owner=${2:-Test-Router}
 build_date=$(TZ=Asia/Shanghai date "+%Y.%m.%d")
 
-echo "（diy.sh）当前编译的源代码来自：${repo}，固件型号是：${owner};"
-echo "（diy.sh）repo的值是：${repo}"
-echo "======================="
-echo "（diy.sh）owner的值是：${owner}"
-echo "======================="
-echo "（diy.sh）build_date的值是：${build_date}"
-echo "======================="
-
 # Modify default IP
 if [ "${owner}" = "Full-Router" ]; then
   sed -i 's/192.168.1.1/192.168.18.1/g' package/base-files/luci2/bin/config_generate
@@ -48,12 +40,20 @@ EOF
 defaultsettings=*/*/default-settings
 [ "${repo}" = "openwrt" ] && language=zh_cn || language=zh_Hans
 
+echo "（diy.sh）zzz-default-settings文件原始内容是："
+cat ${defaultsettings}/files/zzz-default-settings
+echo "=========================================="
+
 # Modify password to Null
 #sed -i '/CYXluq4wUazHjmCDBCqXF/d' ${defaultsettings}/files/zzz-default-settings
 
 # Modify the version number
 sed -i "s/DISTRIB_REVISION='R[0-9]\+\.[0-9]\+\.[0-9]\+'/DISTRIB_REVISION='@R$build_date'/g" ${defaultsettings}/files/zzz-default-settings
 sed -i "s/LEDE/OpenWrt-2410-x64-${owner} by GXNAS build/g" ${defaultsettings}/files/zzz-default-settings
+
+echo "（diy.sh）zzz-default-settings文件修改后的内容是："
+cat ${defaultsettings}/files/zzz-default-settings
+echo "=========================================="
 
 # Modify maximum connections
 sed -i '/customized in this file/a net.netfilter.nf_conntrack_max=165535' package/base-files/files/etc/sysctl.conf
